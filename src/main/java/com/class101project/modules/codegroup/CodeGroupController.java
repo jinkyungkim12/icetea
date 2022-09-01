84a007bd70a458b017b75a428032c8317b3d6f5b
jinkyungkim12 <skwlsrud3@naver.com>
2022-09-01 12:08:29 +0900

    up

--- src/main/java/com/class101project/modules/codegroup/CodeGroupController.java
+++ src/main/java/com/class101project/modules/codegroup/CodeGroupController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/codeGroup/")
+@RequestMapping(value = "/codeGroup/") // /codeGroup/codeGroupList 웹 브라우저 
 public class CodeGroupController {
 
 	@Autowired
