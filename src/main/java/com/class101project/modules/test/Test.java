72863a72150aff9fcbdcb7576e1bb7348e9d85d7
jinkyungkim12 <skwlsrud3@naver.com>
2022-11-02 13:59:48 +0900

    up

---
+++ src/main/java/com/class101project/modules/test/Test.java
@@ -0,0 +1,117 @@
+package com.class101project.modules.test;
+
+public class Test {
+		
+		public Object header;
+		public String resultCode;
+		public String resultMsg;
+
+		public Object body;
+		public String SIGUN_NM;
+		public int SIGUN_CD;
+		public String RESTRT_NM;
+		public String REPRSNT_FOOD_NM;
+		public String TASTFDPLC_TELNO;
+		
+		public Object items;
+		public String REFINE_LOTNO_ADDR;
+		public String REFINE_ROADNM_ADDR;
+		public String REFINE_ZIP_CD;
+		public double REFINE_WGS84_LOGT;
+		public double REFINE_WGS84_LAT;
+		
+		public Object getHeader() {
+			return header;
+		}
+		public void setHeader(Object header) {
+			this.header = header;
+		}
+		public String getResultCode() {
+			return resultCode;
+		}
+		public void setResultCode(String resultCode) {
+			this.resultCode = resultCode;
+		}
+		public String getResultMsg() {
+			return resultMsg;
+		}
+		public void setResultMsg(String resultMsg) {
+			this.resultMsg = resultMsg;
+		}
+		public Object getBody() {
+			return body;
+		}
+		public void setBody(Object body) {
+			this.body = body;
+		}
+		public String getSIGUN_NM() {
+			return SIGUN_NM;
+		}
+		public void setSIGUN_NM(String sIGUN_NM) {
+			SIGUN_NM = sIGUN_NM;
+		}
+		public int getSIGUN_CD() {
+			return SIGUN_CD;
+		}
+		public void setSIGUN_CD(int sIGUN_CD) {
+			SIGUN_CD = sIGUN_CD;
+		}
+		public String getRESTRT_NM() {
+			return RESTRT_NM;
+		}
+		public void setRESTRT_NM(String rESTRT_NM) {
+			RESTRT_NM = rESTRT_NM;
+		}
+		public String getREPRSNT_FOOD_NM() {
+			return REPRSNT_FOOD_NM;
+		}
+		public void setREPRSNT_FOOD_NM(String rEPRSNT_FOOD_NM) {
+			REPRSNT_FOOD_NM = rEPRSNT_FOOD_NM;
+		}
+		public String getTASTFDPLC_TELNO() {
+			return TASTFDPLC_TELNO;
+		}
+		public void setTASTFDPLC_TELNO(String tASTFDPLC_TELNO) {
+			TASTFDPLC_TELNO = tASTFDPLC_TELNO;
+		}
+		public Object getItems() {
+			return items;
+		}
+		public void setItems(Object items) {
+			this.items = items;
+		}
+		public String getREFINE_LOTNO_ADDR() {
+			return REFINE_LOTNO_ADDR;
+		}
+		public void setREFINE_LOTNO_ADDR(String rEFINE_LOTNO_ADDR) {
+			REFINE_LOTNO_ADDR = rEFINE_LOTNO_ADDR;
+		}
+		public String getREFINE_ROADNM_ADDR() {
+			return REFINE_ROADNM_ADDR;
+		}
+		public void setREFINE_ROADNM_ADDR(String rEFINE_ROADNM_ADDR) {
+			REFINE_ROADNM_ADDR = rEFINE_ROADNM_ADDR;
+		}
+		public String getREFINE_ZIP_CD() {
+			return REFINE_ZIP_CD;
+		}
+		public void setREFINE_ZIP_CD(String rEFINE_ZIP_CD) {
+			REFINE_ZIP_CD = rEFINE_ZIP_CD;
+		}
+		public double getREFINE_WGS84_LOGT() {
+			return REFINE_WGS84_LOGT;
+		}
+		public void setREFINE_WGS84_LOGT(double rEFINE_WGS84_LOGT) {
+			REFINE_WGS84_LOGT = rEFINE_WGS84_LOGT;
+		}
+		public double getREFINE_WGS84_LAT() {
+			return REFINE_WGS84_LAT;
+		}
+		public void setREFINE_WGS84_LAT(double rEFINE_WGS84_LAT) {
+			REFINE_WGS84_LAT = rEFINE_WGS84_LAT;
+		}
+		
+//		----------
+	
+		
+}
---
+++ src/main/java/com/class101project/modules/test/TestController.java
@@ -0,0 +1,93 @@
+package com.class101project.modules.test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TestController {
+
+	
+	@RequestMapping(value = "/test/testGyeonggi")
+	public String testGyeonggi(Model model) throws Exception {
+		
+		System.out.println("asdfasdfasdf");
+		
+		String apiUrl = "https://openapi.gg.go.kr/PlaceThatDoATasteyFoodSt";
+		
+		URL url = new URL(apiUrl);		
+		HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+		httpURLConnection.setRequestMethod("GET");
+		
+		BufferedReader bufferedReader;
+		if (httpURLConnection.getResponseCode() >= 200 && httpURLConnection.getResponseCode() <= 300) {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+		} else {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getErrorStream()));
+		}
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		String line;
+		while ((line = bufferedReader.readLine()) != null) {
+			System.out.println("line: " + line);
+			stringBuilder.append(line);
+		}
+
+		bufferedReader.close();
+		httpURLConnection.disconnect();
+
+		System.out.println("stringBuilder.toString(): " + stringBuilder.toString());
+		
+//		json object + array string -> java map
+		
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map = objectMapper.readValue(stringBuilder.toString(), Map.class);
+        
+        System.out.println("######## Map");
+		for (String key : map.keySet()) {
+			String value = String.valueOf(map.get(key));	// ok
+			System.out.println("[key]:" + key + ", [value]:" + value);
+		}
+		
+		Map<String, Object> header = new HashMap<String, Object>();
+		header = (Map<String, Object>) map.get("header");
+		
+		System.out.println("######## Header");
+		for (String key : header.keySet()) {
+			String value = String.valueOf(header.get(key));	// ok
+			System.out.println("[key]:" + key + ", [value]:" + value);
+		}
+		
+//		String aaa = (String) header.get("resultCode");
+		
+//		System.out.println("header.get(\"resultCode\"): " + header.get("resultCode"));
+//		System.out.println("header.get(\"resultMsg\"): " + header.get("resultMsg"));
+		
+		Map<String, Object> body = new HashMap<String, Object>();
+		body = (Map<String, Object>) map.get("body");
+		
+		List<Test> items = new ArrayList<Test>();
+		items = (List<Test>) body.get("items");
+		
+		
+		System.out.println("items.size(): " + items.size());
+		
+//		for(Home item : items) {
+//			System.out.println(item.getMM());
+//		}
+		
+		model.addAllAttributes(header);
+		model.addAllAttributes(body);
+		
+		return "test/testGyeonggi";
+	}
+}
