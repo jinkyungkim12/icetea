0623b171d87512467c4eb890eb63bfc3f0881a7c
jinkyungkim12 <skwlsrud3@naver.com>
2022-11-02 14:02:37 +0900

    up

--- src/main/java/com/class101project/modules/test/TestController.java
+++ src/main/java/com/class101project/modules/test/TestController.java
@@ -17,6 +17,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class TestController {
 
 	
+	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/test/testGyeonggi")
 	public String testGyeonggi(Model model) throws Exception {
 		
