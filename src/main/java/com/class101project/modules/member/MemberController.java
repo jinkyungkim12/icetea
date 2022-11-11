4bd51d2ec829fb8d4923e02db21704b8451c10d3
jinkyungkim12 <skwlsrud3@naver.com>
2022-11-11 16:58:48 +0900

    up

--- src/main/java/com/class101project/modules/member/MemberController.java
+++ src/main/java/com/class101project/modules/member/MemberController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.class101project.common.constants.Constants;
 import com.class101project.common.util.UtilSecurity;
 import com.class101project.modules.code.CodeServiceImpl;
 
