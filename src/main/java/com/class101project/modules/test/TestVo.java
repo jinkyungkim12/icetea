f6c522d5e8261160ac69d281c048d1ace80772f4
jinkyungkim12 <skwlsrud3@naver.com>
2022-11-22 14:06:35 +0900

    up

---
+++ src/main/java/com/class101project/modules/test/TestVo.java
@@ -0,0 +1,50 @@
+package com.class101project.modules.test;
+
+public class TestVo {
+	
+	 private Integer shOption;
+	 private String shValue;
+	 private Integer shDelNy;
+	 
+	 private String shDateStart;
+	 private String shDateEnd;
+	 private Integer shOptionDate;
+	 
+	public Integer getShOption() {
+		return shOption;
+	}
+	public void setShOption(Integer shOption) {
+		this.shOption = shOption;
+	}
+	public String getShValue() {
+		return shValue;
+	}
+	public void setShValue(String shValue) {
+		this.shValue = shValue;
+	}
+	public Integer getShDelNy() {
+		return shDelNy;
+	}
+	public void setShDelNy(Integer shDelNy) {
+		this.shDelNy = shDelNy;
+	}
+	public String getShDateStart() {
+		return shDateStart;
+	}
+	public void setShDateStart(String shDateStart) {
+		this.shDateStart = shDateStart;
+	}
+	public String getShDateEnd() {
+		return shDateEnd;
+	}
+	public void setShDateEnd(String shDateEnd) {
+		this.shDateEnd = shDateEnd;
+	}
+	public Integer getShOptionDate() {
+		return shOptionDate;
+	}
+	public void setShOptionDate(Integer shOptionDate) {
+		this.shOptionDate = shOptionDate;
+	}
+	 
+}
